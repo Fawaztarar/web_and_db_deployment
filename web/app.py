7f4e90a110f0e97169c31ae422b8cbf15b51cac2
Fawaztarar <fawaztarar@gmail.com>
2023-12-15 12:19:15 +0000

    updated message x

--- web/app.py
+++ web/app.py
@@ -90,11 +90,12 @@ def post_message():
     # And redirect to the main page
     return redirect(url_for("get_messages"))
 
+
+
 if __name__ == '__main__':
-    # We also run the server differently depending on the environment.
-    # In production we don't want the fancy error messages — users won't know
-    # what to do with them. So no `debug=True`
-    if os.environ.get("APP_ENV") == "PRODUCTION":
-        app.run(port=5000, host='0.0.0.0')
-    else:
-        app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(
+      debug=True,
+      host="0.0.0.0" # Listen for connections _to_ any server
+    )
+
+
